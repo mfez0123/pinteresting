c724575f5e3394726c09024e62d18f07cd364873
mfez0123 <mfez0123@aol.com>
2013-11-27 16:08:57 -0800

    retry on db:migrate for user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131127225356) do
+ActiveRecord::Schema.define(version: 20131127233135) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
